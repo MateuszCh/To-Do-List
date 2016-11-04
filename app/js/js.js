cc167cebbbdff77a1dfa446a4c8be277b4b4d3bb
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2016-11-04 23:55:05 +0100

    working on addForm part 2

--- app/js/js.js
+++ app/js/js.js
@@ -3,46 +3,23 @@
  */
 var addButton = document.getElementById("plus");
 var lista = document.getElementById("tasks");
-var body = document.getElementsByTagName("body");
-body = body[0];
-
+var addForm = document.getElementsByClassName("addForm");
+addForm = addForm[0];
+var closeButton = document.getElementById("close");
 
 
 addButton.addEventListener("click", function (e) {
-    var addForm = document.createElement("section");
-    var form = document.createElement("form");
-    var title = document.createElement("input");
-    title.type = "text";
-    var content = document.createElement("input");
-    content.type = "text";
-    var data = document.createElement("input");
-    data.type = "date";
-    var czas = document.createElement("input");
-    czas.type = "time";
-    var dodaj = document.createElement("input");
-    dodaj.type = "submit";
-    form.appendChild(title);
-    form.appendChild(content);
-    form.appendChild(data);
-    form.appendChild(czas);
-    form.appendChild(dodaj);
-    var close = document.createElement("i");
-    close.id = "close";
-    close.classList.add("fa");
-    close.classList.add("fa-times");
-    close.classList.add("fa-2x");
-    close.classList.add("close");
-    close.setAttribute("aria-hidden", "true");
-    addForm.classList.add("addForm");
-    body.appendChild(addForm);
+    addForm.style.display = "block";
+    addForm.classList.add("show");
     setTimeout(function () {
-        addForm.appendChild(close);
-        addForm.appendChild(form);
-    }, 100);
-
-    close.addEventListener("click", function (e) {
-        body.removeChild(addForm);
-    }, false)
+        addForm.classList.remove("show");
+    }, 500)
 }, false)
 
-
+closeButton.addEventListener("click", function (e) {
+    addForm.classList.add("hide");
+    setTimeout(function () {
+        addForm.style.display = "none";
+        addForm.classList.remove("hide");
+    }, 500)
+}, false);
